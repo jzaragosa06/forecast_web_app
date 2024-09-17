7fc8e7bc8f12fa9e5df2900d782fecdd88b139d3
leynardguinumtad <lguinumtad_21ur0240@psu.edu.ph>
2024-09-17 11:28:00 +0800

    added css style in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,20 +5,89 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Time Series Analyzer</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"
         integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <style>
+        /* Custom Theme Colors */
+        :root {
+            --primary-color: #1B3B6F;
+            --secondary-color: #21295C;
+            --tertiary-color: #1C7293;
+            --black-color: #000000;
+            --white-color: #FFFFFF;
+        }
 
+        body {
+            background-color: var(--white-color);
+            color: var(--black-color);
+        }
+
+        .navbar {
+            background-color: var(--primary-color);
+            color: var(--white-color);
+        }
+
+        .navbar .navbar-brand,
+        .navbar .btn {
+            color: var(--white-color);
+        }
+
+        .sidebar {
+            background-color: var(--secondary-color);
+            color: var(--white-color);
+        }
+
+        .sidebar .nav-link {
+            color: var(--white-color);
+        }
+
+        .sidebar .nav-link.active {
+            background-color: var(--tertiary-color);
+            color: var(--white-color);
+        }
+
+        .card-header {
+            background-color: var(--primary-color);
+            color: var(--white-color);
+        }
+
+        .btn-primary {
+            background-color: var(--secondary-color);
+            border-color: var(--secondary-color);
+            color: var(--white-color);
+        }
+
+        .btn-primary:hover {
+            background-color: var(--tertiary-color);
+            border-color: var(--tertiary-color);
+            color: var(--white-color);
+        }
+
+        /* Sidebar icons */
+        .nav-item .fas {
+            margin-right: 8px;
+        }
+    </style>
 </head>
 
 <body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-brand" href="#">Time Series Analyzer</a>
+        <div class="ml-auto">
+            <a class="btn btn-outline-light" href="#">Logout</a>
+        </div>
+    </nav>
+
     <div class="container-fluid">
         <div class="row">
-            <!-- Left Side Panel -->
-            <div class="col-md-3 bg-light p-3 min-vh-100 border-end">
+            <!-- Left Sidebar -->
+            <div class="col-md-3 sidebar p-3 min-vh-100 border-end">
                 <!-- User Information -->
                 <div class="text-center mb-4">
                     <img src="{{ Storage::url(Auth::user()->profile_photo) }}" class="rounded-circle"
@@ -26,69 +95,80 @@
                     <h4>{{ Auth::user()->name }}</h4>
                     <p class="text-muted">{{ Auth::user()->email }}</p>
                 </div>
-                <!-- Links -->
+                <!-- Navigation Links -->
                 <ul class="nav flex-column">
                     <li class="nav-item">
-                        <a class="nav-link active" href="#">Dashboard</a>
+                        <a class="nav-link active" href="#"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Profile</a>
+                        <a class="nav-link" href="#"><i class="fas fa-user"></i> Profile</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Settings</a>
+                        <a class="nav-link" href="#"><i class="fas fa-cog"></i> Settings</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Logout</a>
+                        <a class="nav-link" href="#"><i class="fas fa-sign-out-alt"></i> Logout</a>
                     </li>
                 </ul>
             </div>
-            <!-- Right Side Content -->
-            <div class="col-md-9">
-                <div>
-                    <h4>Analyze</h4>
-                    <form action="{{ route('manage.operations') }}" method="post">
-                        @csrf
-                        <div class="form-group">
-                            <label for="file_id">Select File</label>
-                            <select name="file_id" id="file_id" class="form-control">
-                                @foreach (Auth::user()->files as $file)
-                                    <option value="{{ $file->file_id }}">{{ $file->filename }}</option>
-                                @endforeach
-                                <option value="" id="add-more-from-option"> Add more data +</option>
-                            </select>
-                        </div>
+            
+            <!-- Right Content Area -->
+            <div class="col-md-9 p-4">
+                <!-- Analyze Section -->
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <h4>Analyze</h4>
+                    </div>
+                    <div class="card-body">
+                        <form action="{{ route('manage.operations') }}" method="post">
+                            @csrf
+                            <div class="form-group">
+                                <label for="file_id">Select File</label>
+                                <select name="file_id" id="file_id" class="form-control">
+                                    @foreach (Auth::user()->files as $file)
+                                        <option value="{{ $file->file_id }}">{{ $file->filename }}</option>
+                                    @endforeach
+                                    <option value="" id="add-more-from-option"> Add more data +</option>
+                                </select>
+                            </div>
 
-                        <div>
-                            <label for="operation">Select Operation</label>
-                            <select name="operation" id="operation" class="form-control">
-
-                                <option value="trend">Trend</option>
-                                <option value="seasonality">Seasonality</option>
-                                <option value="forecast">Forecast</option>
-                            </select>
-                        </div>
-                        <button type="submit" class="btn btn-secondary">Analyze</button>
-                    </form>
-
+                            <div class="form-group">
+                                <label for="operation">Select Operation</label>
+                                <select name="operation" id="operation" class="form-control">
+                                    <option value="trend">Trend</option>
+                                    <option value="seasonality">Seasonality</option>
+                                    <option value="forecast">Forecast</option>
+                                </select>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Analyze</button>
+                        </form>
+                    </div>
                 </div>
-                <hr>
-                <div>
-                    <h5>List of Input Time Series Data</h5>
-                    @foreach (Auth::user()->files as $file)
-                        <li>
-                            <a href="{{ Storage::url($file->filepath) }}">{{ $file->filename }}</a>
 
-                        </li>
-                    @endforeach
-                    <button type="button" id="ts-info" class="btn btn-primary" data-toggle="modal"
-                        data-target="#ts-info-form">Add More data</button>
+                <!-- List of Input Time Series Data Section -->
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <h5>List of Input Time Series Data</h5>
+                    </div>
+                    <div class="card-body">
+                        <ul>
+                            @foreach (Auth::user()->files as $file)
+                                <li>
+                                    <a href="{{ Storage::url($file->filepath) }}">{{ $file->filename }}</a>
+                                </li>
+                            @endforeach
+                        </ul>
+                        <button type="button" id="ts-info" class="btn btn-primary" data-toggle="modal"
+                            data-target="#ts-info-form">Add More data</button>
+                    </div>
                 </div>
-                <hr>
 
-
-                <div>
-                    <h5>Results (Forecast, Trend, Seasonality Analysis)</h5>
-                    <div class="container">
+                <!-- Results Section -->
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <h5>Results (Forecast, Trend, Seasonality Analysis)</h5>
+                    </div>
+                    <div class="card-body">
                         <ul>
                             @php
                                 $currentFileId = null;
@@ -116,7 +196,7 @@
                                         <form action="{{ route('manage.results', $file->file_assoc_id) }}"
                                             method="post">
                                             @csrf
-                                            <button type="submit">View</button>
+                                            <button type="submit" class="btn btn-secondary">View</button>
                                         </form>
                                     </li>
                                 @else
@@ -131,16 +211,13 @@
                         </ul>
                     </div>
                 </div>
-
-
-
             </div>
         </div>
     </div>
 
+    <!-- Time Series Info Modal -->
     <div class="modal fade" id="ts-info-form" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
-
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -166,85 +243,18 @@
                         <div class="form-group">
                             <label for="freq">Frequency:</label>
                             <select name="freq" class="form-control" required>
-                                <option value="D">Day</option>
-                                <option value="W">Week</option>
-                                <option value="M">Month</option>
-                                <option value="Q">Quarter</option>
+                                <option value="D">Daily</option>
+                                <option value="W">Weekly</option>
+                                <option value="M">Monthly</option>
                                 <option value="Y">Yearly</option>
                             </select>
                         </div>
-                        <div class="form-group">
-                            <label for="description">Description:</label>
-                            <input type="text" name="description" class="form-control">
-                        </div>
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-
                         <button type="submit" class="btn btn-primary">Upload</button>
                     </form>
                 </div>
-
-            </div>
-        </div>
-
-    </div>
-
-
-    <!-- Forecast Modal -->
-    <div class="modal fade" id="forecast-modal" tabindex="-1" role="dialog" aria-labelledby="forecastModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="forecastModalLabel">Forecast Settings</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form action="{{ route('manage.operations') }}" method="POST">
-                        @csrf
-                        <input type="hidden" name="file_id" id="modal_file_id">
-                        <input type="hidden" name="operation" value="forecast">
-
-                        <div class="form-group">
-                            <label for="horizon">Forecast Horizon</label>
-                            <input type="number" name="horizon" id="horizon" class="form-control" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="method">Forecast Method</label><br>
-                            <input type="radio" name="method" value = "with_refit">With
-                            Refit<br>
-                            <input type="radio" name="method" value = "without_refit">Without
-                            Refit <br>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Run Forecast</button>
-                    </form>
-                </div>
             </div>
         </div>
     </div>
-
-    <script>
-        $(document).ready(function() {
-            $('#operation').on('change', function() {
-                if ($(this).val() === 'forecast') {
-                    $('#forecast-modal').modal('show');
-                    $('#modal_file_id').val($('#file_id').val());
-                }
-            });
-
-
-            // Open the 'Add More data' modal when 'Add more data' is selected
-            $('#file_id').on('change', function() {
-                if ($(this).val() === '') {
-                    $('#ts-info-form').modal('show');
-                }
-            });
-
-        });
-    </script>
 </body>
-
-
 
 </html>
