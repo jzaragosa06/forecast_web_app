4df1a3e641bacda6a9b50bb3de4f163f7e085d78
jzaragosa06 <junjunzaragosa2309@gmail.com>
2024-09-26 23:02:48 +0800

    Added the migration and model for storing temporary files relating to sequence alignment.

---
+++ app/Models/SeqalTempFiles.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SeqalTempFiles extends Model
+{
+    use HasFactory;
+
+    protected $table = 'seqal_temp_files';
+
+    // Specify which fields are mass assignable
+    protected $fillable = ['file_id', 'user_id', 'type', 'freq', 'filename', 'description', 'filepath'];
+}
